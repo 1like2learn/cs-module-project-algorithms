42080a5562902eba6aa549ee9a68333cef645fec
1like2learn <ndespained@gmail.com>
2020-09-16 10:29:08 -0700

    single number moving zeroes and product of all numbers done

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,12 +4,29 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
-
-    pass
+    i = 1
+    j = 0
+    while i < len(arr):
+        # print("\ni", arr[i])
+        # print("j", arr[j])
+        # print("arr: ", arr)
+        if arr[i] == arr[j]:
+            # print("two items are the same")
+            arr.pop(i)
+            arr.pop(j)
+            i = 1
+            j = 0
+        else:
+            # print("iterate i")
+            i += 1
+    return arr[j]
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
-
-    print(f"The odd-number-out is {single_number(arr)}")
+    arr2 = [4, 1, 2, 1, 2]
+    arr3 = [2, 2, 1]
+    print(f"The odd-number-out is {single_number(arr)} for arr")
+    # print(f"The odd-number-out is {single_number(arr2)} for arr2")
+    # print(f"The odd-number-out is {single_number(arr3)} for arr3")
