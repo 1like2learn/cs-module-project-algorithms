3de9b9c378f2974f4aa49fe815d624716a42a57a
1like2learn <ndespained@gmail.com>
2020-09-17 11:15:40 -0700

    Eating cookies optimized

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -5,21 +5,14 @@ Returns: an integer
 def single_number(arr):
     # Your code here
     i = 1
-    j = 0
     while i < len(arr):
-        # print("\ni", arr[i])
-        # print("j", arr[j])
-        # print("arr: ", arr)
-        if arr[i] == arr[j]:
-            # print("two items are the same")
+        if arr[i] == arr[0]:
             arr.pop(i)
-            arr.pop(j)
+            arr.pop(0)
             i = 1
-            j = 0
         else:
-            # print("iterate i")
             i += 1
-    return arr[j]
+    return arr[0]
 
 
 if __name__ == '__main__':
@@ -28,5 +21,5 @@ if __name__ == '__main__':
     arr2 = [4, 1, 2, 1, 2]
     arr3 = [2, 2, 1]
     print(f"The odd-number-out is {single_number(arr)} for arr")
-    # print(f"The odd-number-out is {single_number(arr2)} for arr2")
-    # print(f"The odd-number-out is {single_number(arr3)} for arr3")
+    print(f"The odd-number-out is {single_number(arr2)} for arr2")
+    print(f"The odd-number-out is {single_number(arr3)} for arr3")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -4,13 +4,39 @@ Returns: a List of integers
 '''
 def sliding_window_max(nums, k):
     # Your code here
-
-    pass
+    # left = 0
+    # right = k
+    # output = []
+    # while right <= len(nums):
+    #     maxNum = nums[left]
+    #     for i in range(left, right):
+    #         if nums[i] > maxNum:
+    #             maxNum = nums[i]
+    #     output.append(maxNum)
+    #     left += 1
+    #     right += 1
+    # return output
+    left = 0
+    right = k
+    output = []
+    maxNum = left
+    while right <= len(nums):
+        if maxNum < left or maxNum == 0:
+            maxNum = left
+            for i in range(left, right):
+                if nums[i] > nums[maxNum]:
+                    maxNum = i
+        elif nums[maxNum] < nums[right - 1]:
+            maxNum = right - 1
+        output.append(nums[maxNum])
+        left += 1
+        right += 1
+    return output
 
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation 
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
-    k = 3
+    k = 2
 
     print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
